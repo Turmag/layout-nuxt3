974943ff24d61248a53ef06502fb22b380d0c07c
MIUIvanov <MIUIvanov@synergy.ru>
2023-03-05 13:00:26 +0300

    Страница Sandbox

---
+++ components/Sandbox/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="sandbox">
+    <p>Песочница</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Tools',
+};
+</script>
+
+<style scoped lang="scss">
+.sandbox {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  border-left: 10px solid var(--a-white);
+
+  &__logo {
+    position: absolute;
+    top: 0;
+    left: -10px;
+  }
+}
+</style>
