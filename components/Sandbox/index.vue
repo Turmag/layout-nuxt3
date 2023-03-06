65e272e004bc89f3ac3034685dde9360b6ade287
MIUIvanov <MIUIvanov@synergy.ru>
2023-03-06 16:57:25 +0300

    Add Pinia

--- components/Sandbox/index.vue
+++ components/Sandbox/index.vue
@@ -2,7 +2,6 @@
   <section class="sandbox">
     <h1 class="sandbox__title a-font__h1">Vue 3 Nuxt 3</h1>
     <h2 class="sandbox__subtitle a-font__h2">Компоненты и методы</h2>
-    <IconsTelegram />
 
     <div>
       Counter: {{ counter }}
@@ -15,7 +14,7 @@
 <script setup>
 const { $sayWord } = useNuxtApp();
 $sayWord('это проверка работы функции');
-const counter = useState('counter', () => Math.round(Math.random() * 1000));
+const counter = useCounter();
 </script>
 
 <style scoped lang="scss">
