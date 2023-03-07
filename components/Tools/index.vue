370504aac99dacc6f4a9d618967f12cc1590b84f
MIUIvanov <MIUIvanov@synergy.ru>
2023-03-07 17:54:26 +0300

    Оформление шапки в разделе Инструменты

--- components/Tools/index.vue
+++ components/Tools/index.vue
@@ -4,9 +4,20 @@
     <div class="tools__header">
       <ul class="tools__header-list">
         <li class="tools__header-item cell-01">Технологии</li>
-        <li class="tools__header-item cell-02">Vue</li>
+        <li class="tools__header-item cell-02">
+          <div class="tools__header-img-wrapper">
+            <IconsVue class="tools__header-image" />
+          </div>
+          <div class="tools__header-link-group">
+            <a href="https://v3.ru.vuejs.org/" class="tools__header-sourse" target="_blank">Vue.js Doc</a>
+            <button class="tools__header-link">#</button>
+          </div>
+        </li>
         <li class="tools__header-item cell-03">HTML</li>
         <li class="tools__header-item cell-04">CSS</li>
+        <li class="tools__header-item cell-05">CSS</li>
+        <li class="tools__header-item cell-06">CSS</li>
+        <li class="tools__header-item cell-07">CSS</li>
       </ul>
     </div>
     <div class="tools__section">
